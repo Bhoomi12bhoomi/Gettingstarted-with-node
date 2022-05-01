48d72a7136754e05a36eb30627ecebe6b1eb8d01
Bhoomi12bhoomi <87226418+Bhoomi12bhoomi@users.noreply.github.com>
2022-05-01 10:45:33 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,11 +1,12 @@
 
-
+//initialising a server
 const { createServer, request } = require('http');
 const server = createServer((req,res) =>
 {
+    //describing the type of file
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Welcome To Node JS </h1>');
     return res.end();
 });
-
+// the port server works
 server.listen(8080);
